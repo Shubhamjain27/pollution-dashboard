a299a9cf88128c2ff04d25b641a44b2b36aa57be
shubhamjain27 <shubham@Shubhams-MacBook-Air.local>
2019-04-05 21:00:21 +0530

    adding files

---
+++ trial.js
@@ -0,0 +1,8 @@
+const https = require('https')
+const url = 'https://gist.githubusercontent.com/azat-co/a3b93807d89fd5f98ba7829f0557e266/raw/43adc16c256ec52264c2d0bc0251369faf02a3e2/gistfile1.txt'
+https.get(url,(Response) => {
+    let rawdata=''
+    response.on('data', (chunk))
+    rawdata += chunk
+
+})
